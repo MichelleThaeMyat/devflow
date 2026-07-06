f05dbe35284c2e2d1f504c7cb475c2e4e0c9d931
Thae Myat Noe Htut <u6520209@au.edu>
2026-07-06 12:29:27 +0700

    Create a Next.js Application

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>
+          <h1 className="text-3xl">Welcome to the Ultimate Next.js course!</h1>
+        </div>
+    )
+}
+export default Page
